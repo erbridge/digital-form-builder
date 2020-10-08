3835b9fb7d75b7db451c0f2850ffb35e3d54bbaa
Jen <jen+git@cautionyourblast.com>
2020-10-08 10:23:56 +0100

    add translations. use nanoid

--- designer/client/lists-edit.js
+++ designer/client/lists-edit.js
@@ -1,68 +1,68 @@
 import React from 'react'
 import ListEdit from './list-edit'
+import { RenderInPortal } from './components/render-in-portal'
+import Flyout from './flyout'
+import { withI18n } from './i18n'
+import { nanoid } from 'nanoid'
 
 class ListsEdit extends React.Component {
   state = {}
 
   onClickList = (e, list) => {
     e.preventDefault()
-
     this.setState({
-      list
+      list,
+      showAddList: true
     })
   }
 
   onClickAddList = async e => {
     e.preventDefault()
-    const { data } = this.props
-    const id = await data.getId()
     this.setState({
-      id,
+      id: nanoid(6),
       showAddList: true
     })
   }
 
+  closeFlyout = (list) => {
+    this.setState({ showAddList: false })
+  }
+
   render () {
-    const { data } = this.props
+    const { data, i18n } = this.props
     const { lists } = data
-    const { list, id } = this.state
+    const { list, id, showAddList } = this.state
 
     return (
       <div className='govuk-body'>
-        {!list ? (
-          <div>
-            {this.state.showAddList ? (
-              <ListEdit
-                data={data} id={id}
-                onEdit={() => this.setState({ showAddList: false })}
-                onCancel={() => this.setState({ showAddList: false })}
-              />
-            ) : (
-              <ul className='govuk-list'>
-                {lists.map((list, index) => (
-                  <li key={list.name}>
-                    <a href='#' onClick={e => this.onClickList(e, list)}>
-                      {list.title}
-                    </a>
-                  </li>
-                ))}
-                <li>
-                  <hr />
-                  <a href='#' onClick={this.onClickAddList}>Add list</a>
-                </li>
-              </ul>
-            )}
-          </div>
-        ) : (
-          <ListEdit
-            list={list} data={data}
-            onEdit={() => this.setState({ list: null })}
-            onCancel={() => this.setState({ list: null })}
-          />
-        )}
+        <ul className='govuk-list'>
+          {lists.map((list, index) => (
+            <li key={list.name}>
+              <a href='#' onClick={e => this.onClickList(e, list)}>
+                {list.title}
+              </a>
+            </li>
+          ))}
+          <li>
+            <hr />
+            <a href='#' onClick={this.onClickAddList}>Add list</a>
+          </li>
+        </ul>
+        {showAddList &&
+        <RenderInPortal>
+          <Flyout title={list?.name ? i18n('list.editingTitle', { title: list.title }) : i18n('list.newTitle')}
+            onHide={this.closeFlyout} show={showAddList}>
+            <ListEdit
+              data={data} id={id}
+              onEdit={() => this.setState({ showAddList: false })}
+              onCancel={() => this.setState({ showAddList: false })}
+            />
+          </Flyout>
+        </RenderInPortal>
+        }
       </div>
     )
   }
 }
 
-export default ListsEdit
+export default withI18n(ListsEdit)
