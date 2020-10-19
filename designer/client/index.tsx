cbd9285d754f37a36e4ca80ec889a6d31e730485
Jen <jen+git@cautionyourblast.com>
2020-10-19 18:04:13 +0100

    use hook for errors

--- designer/client/index.tsx
+++ designer/client/index.tsx
@@ -1,132 +1,157 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Menu from './menu'
-import Visualisation from './visualisation'
-import NewConfig from './new-config'
-import { Data } from '@xgovformbuilder/model'
-import { customAlphabet } from 'nanoid'
-import { FlyoutContext, DataContext } from './context'
-import './index.scss'
-import { initI18n, i18n } from './i18n'
-import { DesignerApi } from './api/designerApi'
+import React from "react";
+import ReactDOM from "react-dom";
+import Menu from "./menu";
+import Visualisation from "./visualisation";
+import NewConfig from "./new-config";
+import { Data } from "@xgovformbuilder/model";
+import { customAlphabet } from "nanoid";
+import { FlyoutContext, DataContext } from "./context";
+import "./index.scss";
+import { initI18n, i18n } from "./i18n";
+import { DesignerApi } from "./api/designerApi";
 
-initI18n(i18n)
+initI18n(i18n);
 
 /**
  * Custom alphabet is required because '-' is used as a symbol in
  * expr-eval (condition logic) so components which include a '-' in the name
  * result in broken conditions
  */
-const nanoid = customAlphabet('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz', 10)
+const nanoid = customAlphabet(
+  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz",
+  10
+);
 
 export class App extends React.Component {
   state = {
-    id: '',
-    flyoutCount: 0
-  }
+    id: "",
+    flyoutCount: 0,
+  };
 
-  designerApi = new DesignerApi()
+  designerApi = new DesignerApi();
 
-  componentDidMount () {
-    this.setState({ newConfig: window.newConfig ?? false },
-      () => {
-        if (!this.state.loaded && !this.state.newConfig) {
-          this.setState({ id: window.id, previewUrl: window.previewUrl }, () => {
-            const dataPromise = this.designerApi.fetchData(this.state.id)
-            dataPromise.then(data => {
-              this.setState({ loaded: true, data })
-            })
-          })
-        }
-      })
+  componentDidMount() {
+    this.setState({ newConfig: window.newConfig ?? false }, () => {
+      if (!this.state.loaded && !this.state.newConfig) {
+        this.setState({ id: window.id, previewUrl: window.previewUrl }, () => {
+          const dataPromise = this.designerApi.fetchData(this.state.id);
+          dataPromise.then((data) => {
+            this.setState({ loaded: true, data });
+          });
+        });
+      }
+    });
   }
 
   save = (updatedData) => {
-    return window.fetch(`${this.state.id}/api/data`, {
-      method: 'put',
-      // dodgy hack to ensure get methods are called
-      body: JSON.stringify(updatedData),
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      }
-    }).then(res => {
-      if (!res.ok) {
-        throw Error(res.statusText)
-      }
-    }).catch(() => {
-      // Not connected to preview environment
-    }).finally(() => {
-      this.setFunctions(updatedData)
-      this.setState({ data: updatedData, updatedAt: (new Date().toLocaleTimeString()) })
-      return updatedData
-    })
-  }
+    return window
+      .fetch(`${this.state.id}/api/data`, {
+        method: "put",
+        // dodgy hack to ensure get methods are called
+        body: JSON.stringify(updatedData),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        if (!res.ok) {
+          throw Error(res.statusText);
+        }
+      })
+      .catch(() => {
+        // Not connected to preview environment
+      })
+      .finally(() => {
+        this.setFunctions(updatedData);
+        this.setState({
+          data: updatedData,
+          updatedAt: new Date().toLocaleTimeString(),
+        });
+        return updatedData;
+      });
+  };
 
   getId = () => {
-    return nanoid()
-  }
+    return nanoid();
+  };
 
-  setFunctions (data) {
-    data.save = this.save
-    data.getId = this.getId
+  setFunctions(data) {
+    data.save = this.save;
+    data.getId = this.getId;
   }
 
   updateDownloadedAt = (time) => {
-    this.setState({ downloadedAt: time })
-  }
+    this.setState({ downloadedAt: time });
+  };
 
   setStateId = (id) => {
-    this.setState({ id })
-  }
+    this.setState({ id });
+  };
 
   incrementFlyoutCounter = (callback = () => {}) => {
-    let currentCount = this.state.flyoutCount
-    this.setState({ flyoutCount: ++currentCount }, callback())
-  }
+    let currentCount = this.state.flyoutCount;
+    this.setState({ flyoutCount: ++currentCount }, callback());
+  };
 
   decrementFlyoutCounter = (callback = () => {}) => {
-    let currentCount = this.state.flyoutCount
-    this.setState({ flyoutCount: --currentCount }, callback())
-  }
+    let currentCount = this.state.flyoutCount;
+    this.setState({ flyoutCount: --currentCount }, callback());
+  };
 
   saveData = async (toUpdate, callback = () => {}) => {
-    const { id } = this.state
-    const dataJson = await this.designerApi.save(id, toUpdate)
-    const data = new Data(dataJson)
-    this.setState({ data, updatedAt: (new Date().toLocaleTimeString()) }, callback())
-    return toUpdate
-  }
+    const { id } = this.state;
+    const dataJson = await this.designerApi.save(id, toUpdate);
+    this.setState(
+      { data: toUpdate, updatedAt: new Date().toLocaleTimeString() },
+      callback()
+    );
+    console.log("to update", toUpdate);
+    return toUpdate;
+  };
 
-  render () {
-    const { previewUrl, id, flyoutCount, newConfig, data } = this.state
-    const flyoutContextProviderValue = { flyoutCount, increment: this.incrementFlyoutCounter, decrement: this.decrementFlyoutCounter }
-    const dataContextProviderValue = { data, save: this.saveData }
+  render() {
+    const { previewUrl, id, flyoutCount, newConfig, data } = this.state;
+    const flyoutContextProviderValue = {
+      flyoutCount,
+      increment: this.incrementFlyoutCounter,
+      decrement: this.decrementFlyoutCounter,
+    };
+    const dataContextProviderValue = { data, save: this.saveData };
     if (newConfig) {
       return (
-        <div id='app'>
+        <div id="app">
           <NewConfig setStateId={this.setStateId} />
         </div>
-      )
+      );
     }
     if (this.state.loaded) {
       return (
         <DataContext.Provider value={dataContextProviderValue}>
           <FlyoutContext.Provider value={flyoutContextProviderValue}>
-            <div id='app'>
-              <Menu data={data} id={this.state.id} updateDownloadedAt={this.updateDownloadedAt} updatePersona={this.updatePersona} />
-              <Visualisation data={data} downloadedAt={this.state.downloadedAt} updatedAt={this.state.updatedAt} persona={this.state.persona} id={id} previewUrl={previewUrl} />
+            <div id="app">
+              <Menu
+                data={data}
+                id={this.state.id}
+                updateDownloadedAt={this.updateDownloadedAt}
+                updatePersona={this.updatePersona}
+              />
+              <Visualisation
+                data={data}
+                downloadedAt={this.state.downloadedAt}
+                updatedAt={this.state.updatedAt}
+                persona={this.state.persona}
+                id={id}
+                previewUrl={previewUrl}
+              />
             </div>
           </FlyoutContext.Provider>
         </DataContext.Provider>
-      )
+      );
     } else {
-      return <div>Loading...</div>
+      return <div>Loading...</div>;
     }
   }
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById("root"));
--- designer/client/list/list-item-edit.js
+++ designer/client/list/list-item-edit.js
@@ -1,92 +1,147 @@
-import React, { useContext, useEffect, useLayoutEffect, useState } from 'react'
-import { withI18n } from './../i18n'
-import { Input } from '@govuk-jsx/input'
-import { Textarea } from '@govuk-jsx/textarea'
-import { Label } from '@govuk-jsx/label'
-import { Hint } from '@govuk-jsx/hint'
-import { RenderInPortal } from '../components/render-in-portal'
-import Flyout from '../flyout'
-import ComponentCreate from '../component-create'
-import { ComponentTypes } from '@xgovformbuilder/model'
-import { DataContext } from '../context'
+import React, { useContext, useEffect, useLayoutEffect, useState } from "react";
+import { withI18n } from "./../i18n";
+import { Input } from "@govuk-jsx/input";
+import { Textarea } from "@govuk-jsx/textarea";
+import { Label } from "@govuk-jsx/label";
+import { Hint } from "@govuk-jsx/hint";
+import { ErrorMessage } from "@govuk-jsx/error-message";
+import { RenderInPortal } from "../components/render-in-portal";
+import Flyout from "../flyout";
+import ComponentCreate from "../component-create";
+import { ComponentTypes } from "@xgovformbuilder/model";
+import { DataContext } from "../context";
+import { useRequiredInput } from "../handlers/required";
 
-export function ListItemEdit (props) {
-  const { data, save } = useContext(DataContext)
-  const [title, setTitle] = useState(props.selectedItem?.text ?? '')
-  const [helpText, setHelpText] = useState(props.selectedItem?.description ?? '')
-  const [value, setValue] = useState(props.selectedItem?.value ?? '')
-  const [condition, setCondition] = useState(props.selectedItem?.condition ?? '')
-  const [isEditingSubComponent, setIsEditingSubComponent] = useState(false)
-  const { i18n, list: selectedList } = props
-  const editingItemAtIndex = selectedList.items?.findIndex(item => item.text === title)
-  const { conditions } = data
-  const allowedTypes = ComponentTypes.filter(type => !['RadiosField', 'CheckboxesField', 'SelectField', 'AutocompleteField', 'YesNoField'].includes(type.name))
+export function ListItemEdit(props) {
+  const { data, save } = useContext(DataContext);
+  const [title, setTitle] = useState(props.selectedItem?.text ?? "");
+  const [helpText, setHelpText] = useState(
+    props.selectedItem?.description ?? ""
+  );
+  const [value, setValue] = useState(props.selectedItem?.value ?? "");
+
+  const [condition, setCondition] = useState(
+    props.selectedItem?.condition ?? ""
+  );
+  const [isEditingSubComponent, setIsEditingSubComponent] = useState(false);
+  const { i18n, list: selectedList } = props;
+  const editingItemAtIndex = selectedList.items?.findIndex(
+    (item) => item.text === title
+  );
+  const { conditions } = data;
+  const allowedTypes = ComponentTypes.filter(
+    (type) =>
+      ![
+        "RadiosField",
+        "CheckboxesField",
+        "SelectField",
+        "AutocompleteField",
+        "YesNoField",
+      ].includes(type.name)
+  );
 
   const handleCreateSubComponent = (e) => {
-    e.preventDefault()
-    setIsEditingSubComponent(true)
-  }
+    e.preventDefault();
+    setIsEditingSubComponent(true);
+  };
 
-  const handleSubmit = async e => {
-    e.preventDefault()
+  const handleChangeValue = (e) => {
+    const value = e.target.value;
+    setValue(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     const listItem = {
-      text: title, description: helpText, value, condition
-    }
+      text: title,
+      description: helpText ?? "",
+      value,
+      condition: condition ?? "",
+    };
 
     if (editingItemAtIndex === -1) {
-      const items = data.lists.find(list => list.name === selectedList.name).items
-      data.lists.find(list => list.name === selectedList.name).items = [...items, listItem]
+      console.log("editing at item", "section", selectedList);
+      const items =
+        data.lists.find((list) => list.name === selectedList.name)?.items ?? [];
+      data.lists.find((list) => list.name === selectedList.name).items = [
+        ...items,
+        listItem,
+      ];
     } else {
-      data.lists.find(list => list.name === selectedList.name).items[editingItemAtIndex] = listItem
+      data.lists.find((list) => list.name === selectedList.name).items[
+        editingItemAtIndex
+      ] = listItem;
     }
 
-    await save(data)
-  }
+    await save(data);
+  };
+
+  const { fieldValue, hasError, handleFieldChange } = useRequiredInput();
+  const error = hasError ? { children: [i18n("errors.required")] } : undefined;
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <Input label={{
-          className: 'govuk-label--s',
-          children: [i18n('list.item.title')]
-        }}
-        value={title}
-        onChange={e => setTitle(e.target.value)}/>
-        <Textarea
-          label={{ children: [i18n('list.item.titleHint')] }}
-          value={helpText}
-          onChange={e => setHelpText(e.target.value)}
+        <Input
+          label={{
+            className: "govuk-label--s",
+            children: [i18n("list.item.title")],
+          }}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
-        <Input label={{ children: [i18n('list.item.value')] }}
-          hint={{ children: [i18n('list.item.valueHint')] }}
-          value={value}
-          onChange={e => setValue(e.target.value)}/>
+        <Textarea
+          label={{ children: [i18n("list.item.titleHint")] }}
+          value={helpText}
+          onChange={(e) => setHelpText(e.target.value)}
+        />
+        <Input
+          label={{ children: [i18n("list.item.value")] }}
+          hint={{ children: [i18n("list.item.valueHint")] }}
+          value={fieldValue}
+          errorMessage={error}
+          onChange={(e) => handleFieldChange(e)}
+        />
 
-        <Label>{i18n('list.item.conditions')}</Label>
-        <Hint>{i18n('list.item.conditionsHint')}</Hint>
-        <select className='govuk-select' id='condition' name='options.condition' value={condition} onChange={event => setCondition(event.target.value)}>
-          <option value='' />
-          {conditions?.map((condition) => (<option key={condition.name} value={condition}>{condition.name}</option>))}
+        <Label>{i18n("list.item.conditions")}</Label>
+        <Hint>{i18n("list.item.conditionsHint")}</Hint>
+        <select
+          className="govuk-select"
+          id="condition"
+          name="options.condition"
+          value={condition}
+          onChange={(event) => setCondition(event.target.value)}
+        >
+          <option value="" />
+          {conditions?.map((condition) => (
+            <option key={condition.name} value={condition}>
+              {condition.name}
+            </option>
+          ))}
         </select>
-        <hr/>
-        <p className="govuk-body govuk-!-font-weight-bold govuk-!-margin-0">{i18n('list.item.subComponent')}</p>
-        <Hint>{i18n('list.item.subComponentHint')}</Hint>
-        <a href="#" className="govuk-link" onClick={handleCreateSubComponent}>{i18n('list.item.createSubComponent')}</a>
-        {isEditingSubComponent &&
-        <RenderInPortal>
-          <Flyout width={'xlarge'} show={isEditingSubComponent}>
-            <ComponentCreate allowedTypes={allowedTypes}/>
-          </Flyout>
-        </RenderInPortal>
-        }
-        <div className={'govuk-form-group'}>
-          <button className='govuk-button' type='submit'>Save</button>
+        <hr />
+        <p className="govuk-body govuk-!-font-weight-bold govuk-!-margin-0">
+          {i18n("list.item.subComponent")}
+        </p>
+        <Hint>{i18n("list.item.subComponentHint")}</Hint>
+        <a href="#" className="govuk-link" onClick={handleCreateSubComponent}>
+          {i18n("list.item.createSubComponent")}
+        </a>
+        {isEditingSubComponent && (
+          <RenderInPortal>
+            <Flyout width={"xlarge"} show={isEditingSubComponent}>
+              <ComponentCreate allowedTypes={allowedTypes} />
+            </Flyout>
+          </RenderInPortal>
+        )}
+        <div className={"govuk-form-group"}>
+          <button className="govuk-button" type="submit">
+            Save
+          </button>
         </div>
       </form>
-
     </div>
-
-  )
+  );
 }
 
-export default withI18n(ListItemEdit)
+export default withI18n(ListItemEdit);
