9188abb81222c34df97e834b37ac2cbf4ee33c39
jenbutongit <jen+git@cautionyourblast.com>
2020-12-18 10:55:35 +0000

    test case

--- designer/test/componentReducer.test.tsx
+++ designer/test/componentReducer.test.tsx
@@ -1,18 +1,19 @@
-import sinon from "sinon";
-import React from "react";
-import { shallow, mount } from "enzyme";
 import * as Code from "@hapi/code";
 import * as Lab from "@hapi/lab";
 import {
   componentReducer,
   getSubReducer,
 } from "../client/reducers/component/componentReducer";
-import { Actions, Meta } from "../client/reducers/component/types";
-import { metaReducer } from "../client/reducers/component/componentReducer.meta";
-import { optionsReducer } from "../client/reducers/component/componentReducer.options";
-import { fieldsReducer } from "../client/reducers/component/componentReducer.fields";
-import { schemaReducer } from "../client/reducers/component/componentReducer.schema";
-import { componentListReducer } from "../client/reducers/component/componentReducer.list";
+import { Actions } from "../client/reducers/component/types";
+import {
+  metaReducer,
+  optionsReducer,
+  fieldsReducer,
+  schemaReducer,
+  componentListReducer,
+  componentListItemReducer,
+} from "../client/reducers/component";
+
 const { expect } = Code;
 const lab = Lab.script();
 exports.lab = lab;
@@ -24,13 +25,15 @@ suite("Component reducer", () => {
       schemaAction = Actions.EDIT_SCHEMA_MIN,
       fieldsAction = Actions.EDIT_TITLE,
       optionsAction = Actions.EDIT_OPTIONS_HIDE_TITLE,
-      listAction = Actions.EDIT_LIST;
+      listAction = Actions.EDIT_LIST,
+      listItemAction = Actions.EDIT_VALUE;
 
     expect(getSubReducer(metaAction)).to.equal(metaReducer);
     expect(getSubReducer(schemaAction)).to.equal(schemaReducer);
     expect(getSubReducer(optionsAction)).to.equal(optionsReducer);
     expect(getSubReducer(fieldsAction)).to.equal(fieldsReducer);
     expect(getSubReducer(listAction)).to.equal(componentListReducer);
+    expect(getSubReducer(listItemAction)).to.equal(componentListItemReducer);
   });
 
   test("componentReducer adds hasValidated flag correctly", () => {
